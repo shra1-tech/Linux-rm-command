4d004d09ceee09539db4b7d44baa4eeb99544c45
Shravan Marati <coolshra1@gmail.com>
2025-09-07 18:59:23 +0530

    Update rm-i.sh

--- rm-i.sh
+++ rm-i.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-##Save this script as rm and make it alias
+##Save this script as rm-i.sh and make it alias to rm command
 ##provide 755 permissions using chmod 755 "rm-i.sh" & cp to /usr/bin/rm-i.sh
 ##alias rm=/usr/bin/rm-i.sh make this script as alias to rm command.
 ##you can watch my yt vid for better understanding: "https://youtu.be/-sloFL0fWS4?si=QvHXogAcsRvGZpxI"
