c3725d467af937f8161c27c4edd8d0a1b89e1cc5
Shravan Marati <coolshra1@gmail.com>
2025-09-07 18:58:21 +0530

    rm command
    removing rf switches from rm command.

---
+++ rm-i.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+##Save this script as rm and make it alias
+##provide 755 permissions using chmod 755 "rm-i.sh" & cp to /usr/bin/rm-i.sh
+##alias rm=/usr/bin/rm-i.sh make this script as alias to rm command.
+##you can watch my yt vid for better understanding: "https://youtu.be/-sloFL0fWS4?si=QvHXogAcsRvGZpxI"
+
+if [ "{$1}" == "{$rf}" ]
+  then
+      echo "removing rf swiches from the command" 
+	  shift 1
+	  #shift 1 removes first argument
+	  /usr/bin/rm -i $@
+  else
+     /usr/bin/rm -i $@
+fi
+
